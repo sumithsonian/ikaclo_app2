119d9dd363857c102772a657a9a41522f475f01b
角雄崇 <sumi@ini.co.jp>
2020-11-24 02:27:23 +0900

    [update]layout調整

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer :absolute="!fixed" app>
+    <v-container>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-container>
+  </v-footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-header>
+    <v-container>
+      <v-app-bar app></v-app-bar>
+    </v-container>
+  </v-header>
+</template>
